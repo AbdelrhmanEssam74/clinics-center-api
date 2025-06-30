40a05b83cbc5334f492dd20b90c66e18845e9e2d
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-30 09:03:18 +0300

    Update `HomeController` to fetch doctor details with related data by ID

--- app/Http/Controllers/API/Doctor/HomeController.php
+++ app/Http/Controllers/API/Doctor/HomeController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\API\Doctor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function APITest()
+    public function home($id)
     {
+        $doctor = Doctor::with(['specialty', 'appointments' , 'user', 'user.role'])
+            ->find($id);
+        if (!$doctor) {
+            return response()->json(['message' => 'Doctor not found'], 404);
+        }
         return response()->json([
-            'message' => 'API is working',
-            'status' => 'success'
-        ]);
+            'doctor' => $doctor
+        ], 200);
     }
 }
