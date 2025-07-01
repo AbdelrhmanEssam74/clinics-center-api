4632e5c0ada81cc58f4f15d5839cc25a92d88602
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-07-01 16:37:07 +0300

    add users

---
+++ app/Http/Controllers/users/UserController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\users;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $query = User::query();
+
+
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->name . '%');
+        }
+
+        $users = $query->get();
+
+        return UserResource::collection($users);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $data['image'] = $request->file('image')->store('users', 'public');
+        }
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $data = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $data['image'] = $request->file('image')->store('users', 'public');
+        }
+
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        $user->update($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        if(!$user){
+            return response().join(['massage'=>"User Not Found"]);
+
+        }
+        else{
+        $user->delete();
+
+        return response()->json(['massage'=>"user deleted sucssfully"], 200);
+
+        }
+    }
+
+
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
+        'image',
+        'profile_description',
+        'role_id',
         'password',
     ];
 
